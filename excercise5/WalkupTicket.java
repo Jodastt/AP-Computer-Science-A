43938fe3495f9cd75cc268799d5cd885a5c551cc
Joseph S <38223151+Jodastt@users.noreply.github.com>
2020-02-13 12:54:34 -0800

    Create WalkupTicket.java

---
+++ excercise5/WalkupTicket.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+
+public class WalkupTicket extends Ticket {
+    private int number;
+    private int price;
+    public WalkupTicket(){
+        super((int) Math.random() * 10, 50);
+    }
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public void printTicket() {
+        super.printTicket();
+    }
+}
