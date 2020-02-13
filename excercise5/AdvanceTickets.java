27125a7b8f8c7deede37d155155ee7225dd334c8
Joseph S <38223151+Jodastt@users.noreply.github.com>
2020-02-13 12:54:48 -0800

    Create AdvanceTickets.java

---
+++ excercise5/AdvanceTickets.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+public class AdvanceTickets extends Ticket{
+        super((int) Math.random() * 10, thePrice);
+        public AdvanceTickets(int days){
+        int thePrice = -1;
+        if(days > 9){
+            thePrice = 30;
+        } else {
+            thePrice = 40;
+        }
+    }
+}
